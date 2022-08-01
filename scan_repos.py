dad2f08644a0ba94a82dcd9a78432b69aeff614d
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-07-31 23:49:58 -0500

    Skip new db, moved code blocks to functions

--- scan_repos.py
+++ scan_repos.py
@@ -13,18 +13,16 @@ from sh import gh
 # verbose icecream
 ic.configureOutput(includeContext=True)
 
-'''
-Scan Repos
-
-Call gh CLI to get a user's repo URLs,
-then feed to TinyDB JSON,
-scan w/___ red team tool (e.g., trufflehog)
-'''
-
 home = Path.home()
 env = Path('.env')
 cwd = Path.cwd()
+db_dir = cwd/'raw'
+fn = Path(f"{cwd}/raw/repos.json")
 
+# create directory if it doesn't exist
+Path.mkdir(db_dir, exist_ok=True)
+
+# env vars
 if env.exists():
     username = config('USERNAME', default='', cast=str)
     limit = config('LIMIT', default=5, cast=int)
@@ -34,27 +32,52 @@ else:
     limit = os.getenv('LIMIT')
     visibility = os.getenv('VISIBILITY')    # public, private, internal
 
-raw = gh("repo", "list", username, "--limit", limit, "--visibility", visibility, "--json", "url")
 
-# convert to utf-8
-pub_repos = raw.stdout.decode('utf-8')
+def get_repos(username, limit, visibility):
+    '''Get the repos for a user'''
+    if not fn.exists():
+        raw = gh("repo", "list", username, "--limit", limit, "--visibility", visibility, "--json", "url")
 
-# strip ansi escape codes
-# * https://stackoverflow.com/a/14693789
-ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-pub_repos = ansi_escape.sub('', pub_repos)
+        # convert to utf-8
+        pub_repos = raw.stdout.decode('utf-8')
 
-# df extract repo url
-df = pd.read_json(pub_repos)
-df = df[df['url'].str.startswith('https://')]
+        # strip ansi escape codes
+        # * https://stackoverflow.com/a/14693789
+        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+        pub_repos = ansi_escape.sub('', pub_repos)
 
-# create directory if it doesn't exist
-db_dir = cwd/'raw'
-Path.mkdir(db_dir, exist_ok=True)
+        # extract repo url
+        df = pd.read_json(pub_repos)
+        df = df[df['url'].str.startswith('https://')]
+    else:
+        df = pd.read_json(fn)
 
-# store the repo names as a tinydb database
-db = tinydb.TinyDB(Path(f"{db_dir}/repos.json"))
-db.insert(json.loads(df.to_json()))
-ic(db.all())
+    return df
+
+
+def write_repos(fn, df=None):
+    '''Store the repo names as a tinydb database'''
+    if not fn.exists():
+        db = tinydb.TinyDB(Path(fn))
+        db.insert(json.loads(df.to_json()))
+    else:
+        db = tinydb.TinyDB(Path(fn))
+
+    return db
+
+
+def read_repos(db):
+    '''Read the repo names from the tinydb database'''
+    return ic(db.all())
+
+
+def main():
+    df = get_repos(username, limit, visibility)
+    db = write_repos(fn, df)
+
+    return read_repos(db)
+
 
 # TODO: scan repo URLs
+if __name__ == "__main__":
+    main()
